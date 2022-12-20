726e9e1dc0eb6a3947224f9fffcc59140395ffde
jhun1020 <jhun1020.kh@gmail.com>
2022-12-20 19:16:18 +0900

    first commit

---
+++ src/main/java/com/jhunsuvlets/toysuvlets2/Toysuvlets2Application.java
@@ -0,0 +1,13 @@
+package com.jhunsuvlets.toysuvlets2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toysuvlets2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toysuvlets2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhunsuvlets/toysuvlets2/Toysuvlets2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhunsuvlets.toysuvlets2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toysuvlets2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
