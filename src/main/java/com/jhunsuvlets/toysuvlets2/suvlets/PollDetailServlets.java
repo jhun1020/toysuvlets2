bf05af8f8b4b0a990c1cd90bda71c7df171cfa28
jhun1020 <jhun1020.kh@gmail.com>
2022-12-27 15:35:05 +0900

    aa

--- src/main/java/com/jhunsuvlets/toysuvlets2/suvlets/PollDetailServlets.java
+++ src/main/java/com/jhunsuvlets/toysuvlets2/suvlets/PollDetailServlets.java
@@ -16,7 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/poll/PollServlet")
-public class DetailServlets extends HttpServlet{
+public class PollDetailServlets extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
